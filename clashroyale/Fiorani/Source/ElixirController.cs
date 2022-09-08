f7858d48700ff33913a15e0487fe1fdb0dcd774a
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2022-09-08 15:12:06 +0200

    add ElixirController

---
+++ clashroyale/Fiorani/Source/ElixirController.cs
@@ -0,0 +1,53 @@
+﻿namespace Fiorani.Source;
+
+public class ElixirController
+{
+    private static Timer? _timer;
+    private static int _elixir;
+    private static bool _run;
+    private static readonly ConsoleColor DefaultC = Console.ForegroundColor;
+
+    public ElixirController()
+    {
+        Console.WriteLine("Press R to Start the Timer " + Environment.NewLine);
+        _elixir = 0;
+        _run = true;
+        RunningThread();
+    }
+
+    private static void Run()
+    {
+        while (_run)
+        {
+            if (_elixir < 10)
+            {
+                _elixir++;
+            }
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Time! " + _elixir);
+        }
+    }
+
+    static void RunningThread()
+    {
+        Thread thread = new Thread(Run);
+        thread.IsBackground = false;
+        thread.Start();
+    }
+
+    public int GetElixir()
+    {
+        return _elixir;
+    }
+
+    public void SetEixir()
+    {
+        _elixir = 0;
+    }
+
+    public void SetRunFalse()
+    {
+        _run = false;
+    }
+}
