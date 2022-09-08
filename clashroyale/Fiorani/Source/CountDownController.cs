1d1e85a99ffe7b1972be978f2b24fbe70ad55025
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2022-09-08 15:11:48 +0200

    add countDownController

---
+++ clashroyale/Fiorani/Source/CountDownController.cs
@@ -0,0 +1,53 @@
+﻿namespace Fiorani.Source;
+
+public class CountDownController
+{
+    private static Timer? _timer;
+    private static int _time;
+    private const int DefaultTime = 90;
+    private static bool _run;
+    private static readonly ConsoleColor DefaultC = Console.ForegroundColor;
+
+    public CountDownController()
+    {
+        Console.WriteLine("Press R to Start the Timer " + Environment.NewLine);
+        _time = 90;
+        _run = true;
+        RunningThread();
+    }
+    private static void Run()
+    {
+        while (_run)
+        {
+            if (_time > 0)
+            {
+                _time--;
+            }
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Time! " + _time);
+        }
+    }
+
+    static void RunningThread()
+    {
+        Thread thread = new Thread(Run);
+        thread.IsBackground = false;
+        thread.Start();
+    }
+
+    public int GetTime()
+    {
+        return _time;
+    }
+
+    public void SetTime()
+    {
+        _time = DefaultTime;
+    }
+
+    public void SetRunFalse()
+    {
+        _run = false;
+    }
+}
