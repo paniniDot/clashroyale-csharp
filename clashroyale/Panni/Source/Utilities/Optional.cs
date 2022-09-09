39975dc6b80e980c02e610d0340a0a4592e9dd53
Mattia Panni <mattia.panni@studio.unibo.it>
2022-09-09 17:56:37 +0200

    Add a simple c# equivalent of java.util.Optional

---
+++ clashroyale/Panni/Source/Utilities/Optional.cs
@@ -0,0 +1,26 @@
+﻿namespace Panni.Source.Utilities;
+public class Optional<T> {
+    private T _value;
+    public bool IsPresent { get; private set; }
+
+    private Optional() { }
+
+    public static Optional<T> Empty() {
+        return new Optional<T>();
+    }
+
+    public static Optional<T> Of(T value) {
+        var obj = new Optional<T>();
+        obj.Set(value);
+        return obj;
+    }
+
+    public void Set(T value) {
+        this._value = value;
+        IsPresent = true;
+    }
+
+    public T Get() {
+        return _value;
+    }
+}
