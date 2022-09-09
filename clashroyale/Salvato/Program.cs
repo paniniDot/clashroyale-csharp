936155f2c3f27f0822650871802c593c259982e1
Alexandro Salvato <salvato.alexandro@gmail.com>
2022-09-09 16:59:23 +0200

    Create Salvato project

---
+++ clashroyale/Salvato/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Salvato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
