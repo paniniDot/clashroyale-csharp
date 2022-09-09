86c17fe080993f42f77a4bdeff0dd9ad7bb6cfca
Mattia Panni <mattia.panni@studio.unibo.it>
2022-09-09 17:59:00 +0200

    Add Card class

---
+++ clashroyale/Panni/Source/Model/Cards/Card.cs
@@ -0,0 +1,52 @@
+﻿using Panni.Source.Model.Users;
+using Panni.Source.Utilities;
+
+namespace Panni.Source.Model.Cards;
+
+public abstract class Card : IAttackable
+{
+    public int Cost { get; }
+    public Vector2 Position { get; set; }
+    public double Range { get; }
+    public double CurrentHp { get; private set; }
+    public User Owner { get; }
+    public double Damage { get; }
+    public Optional<IAttackable> CurrentTarget { get; private set; }
+
+    protected Card(int cost, Vector2 position, User owner, double maxHp, double damage, double range)
+    {
+        this.Cost = cost;
+        this.Owner = owner;
+        this.Position = position;
+        this.CurrentHp = maxHp;
+        this.Damage = damage;
+        this.Range = range;
+        this.CurrentTarget = Optional<IAttackable>.Empty();
+    }
+
+    public void SetCurrentTarget(IAttackable target)
+    {
+        this.CurrentTarget = Optional<IAttackable>.Of(target);
+    }
+
+    public void ResetCurrentTarget()
+    {
+        this.CurrentTarget = Optional<IAttackable>.Empty();
+    }
+
+    public void ReduceHpBy(double damage)
+    {
+        this.CurrentHp = this.CurrentHp < damage ? 0 : this.CurrentHp - damage;
+    }
+
+    public void AttackCurrentTarget()
+    {
+        if (this.CurrentTarget.IsPresent)
+            this.CurrentTarget.Get().ReduceHpBy(this.Damage);
+    }
+
+    public bool IsDead()
+    {
+        return this.CurrentHp <= 0;
+    }
+}
