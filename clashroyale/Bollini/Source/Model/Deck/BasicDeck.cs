a6035d17fd001b94e48e163b95942d0daa790c5e
SimoneBollini <simone.bollini@studio.unibo.it>
2022-09-12 16:41:27 +0200

    added classes made in java

--- clashroyale/Bollini/Source/Model/Deck/BasicDeck.cs
+++ clashroyale/Bollini/Source/Model/Deck/BasicDeck.cs
@@ -2,5 +2,37 @@
 
 public class BasicDeck
 {
-    
+    private static final int HEIGHTCARD = 100;
+    private static final int POSCARD1 = 200;
+    private static final int POSCARD2 = 300;
+    private static final int POSCARD3 = 400;
+    private static final int POSCARD4 = 500;
+
+    private final Set<Vector2> positionFree;
+
+    /**
+   * Constructor for position free, protected in order to cannot instantiate it from outside its package. 
+   */
+    protected BasicDeck() {
+        this.positionFree = new HashSet<>(Arrays.asList(new Vector2(POSCARD1, HEIGHTCARD), new Vector2(POSCARD2, HEIGHTCARD), new Vector2(POSCARD3, HEIGHTCARD), new Vector2(POSCARD4, HEIGHTCARD)));
+    }
+
+    /**
+   * 
+   * @return positionFree
+   */
+    public Set<Vector2> getPositionFree() {
+        return positionFree;
+    }
+
+    /**
+   * 
+   * @return the first free position and deletes it from those available
+   */
+    public Vector2 newPositionFree() {
+        final Iterator<Vector2> i = getPositionFree().iterator();
+        final Vector2 tmp  =  i.next();
+        getPositionFree().remove(tmp);
+        return tmp;
+    }
 }
--- clashroyale/Bollini/Source/Model/Deck/PlayersDeck.cs
+++ clashroyale/Bollini/Source/Model/Deck/PlayersDeck.cs
@@ -1,6 +1,115 @@
 ﻿namespace Bollini.Source.Model.Deck;
 
-public class PlayersDeck
-{
-    
+public final class PlayersDeck extends BasicDeck {
+
+  private static final PlayersDeck DECK = new PlayersDeck();
+  private final Map<String, Card> deckMap;
+  private final Map<String, Card> cardsMap;
+
+  /**
+   * initialize basic cards and decks.
+   */
+  private PlayersDeck() {
+    this.deckMap = new HashMap<>();
+    this.deckMap.put("Barbarian", Barbarian.create(GlobalData.USER, newPositionFree()));
+    this.deckMap.put("Giant", Giant.create(GlobalData.USER, newPositionFree()));
+    this.deckMap.put("InfernoTower", InfernoTower.create(GlobalData.USER, newPositionFree()));
+    this.deckMap.put("Wizard", Wizard.create(GlobalData.USER, newPositionFree())); 
+    this.cardsMap = new HashMap<>();
+    this.cardsMap.put("Archer", Archer.create(GlobalData.USER, new Vector2(0, 0)));
+    this.cardsMap.put("MiniPekka", MiniPekka.create(GlobalData.USER, new Vector2(0, 0)));
+    this.cardsMap.put("Valkrie", Valkyrie.create(GlobalData.USER, new Vector2(0, 0)));
+
+  }
+
+  /**
+   * 
+   * @return getDeckMap
+   */
+  public Map<String, Card> getDeck() {
+    return deckMap;
+  }
+
+  /**
+   * 
+   * @return getCardsMap
+   */
+  public Map<String, Card> getCards() {
+    return cardsMap;
+  }
+
+  /**
+   * 
+   * @param select
+   * @return deckMap
+   */
+  public Map<String, Card> addDeck(final String select) {
+    deckMap.put(select, cardsMap.get(select));
+    return deckMap;
+  }
+  /**
+   * 
+   * @param select
+   * @return cardsMap
+   */
+  public Map<String, Card> addCard(final String select) {
+    cardsMap.put(select, deckMap.get(select));
+    return cardsMap;
+  }
+  /**
+   * 
+   * @param select
+   * @return cardsMap
+   */
+  public Map<String, Card> removeCard(final String select) {
+    cardsMap.remove(select);
+    return cardsMap;
+  }
+
+  /**
+   * 
+   * @param select
+   * @return deckMap
+   */
+  public Map<String, Card> removeDeckCard(final String select) {
+    deckMap.remove(select);
+    return deckMap;
+  }
+
+  /**
+   * 
+   * @return List<Card> from map
+   */
+  public List<Card> cardList() {
+    return deckMap.entrySet().stream()
+        .map(e -> e.getValue())
+        .collect(Collectors.toList());
+  }
+  /**
+   * 
+   * @return List<String> from key deckmap
+   */
+  public List<String> namesCardsDeck() {
+    return deckMap.entrySet().stream()
+        .map(e -> e.getKey())
+        .collect(Collectors.toList());
+  }
+  /**
+   * 
+   * @return List<String> from key cardsmap
+   */
+  public List<String> namesCardsCard() {
+    return cardsMap.entrySet().stream()
+        .map(e -> e.getKey())
+        .collect(Collectors.toList());
+  }
+
+  /**
+   * 
+   * 
+   * @return the only DECK
+   */
+  public static PlayersDeck getInstance() {
+    return DECK;
+  }
 }
