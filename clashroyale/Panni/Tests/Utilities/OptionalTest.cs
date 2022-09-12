71ad6b89014c73f160dd617c16e55a3c7c7176e3
Mattia Panni <mattia.panni@studio.unibo.it>
2022-09-12 12:16:18 +0200

    Add test for Optional

---
+++ clashroyale/Panni/Tests/Utilities/OptionalTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Panni.Source.Utilities;
+
+namespace Panni.Tests.Utilities;
+
+public class OptionalTest
+{
+    private Optional<int> _optional;
+
+    [SetUp]
+    public void SetUpOptional()
+    {
+        this._optional = Optional<int>.Empty();
+    }
+
+    [Test]
+    public void EmptyOptionalTest()
+    {
+        Assert.False(this._optional.IsPresent);
+    }
+
+    [Test]
+    public void ValuatedOptionalTest()
+    {
+        this._optional = Optional<int>.Of(3);
+        Assert.IsTrue(this._optional.IsPresent);
+        Assert.That(this._optional.Get(), Is.EqualTo(3));
+    }
+    
+}
