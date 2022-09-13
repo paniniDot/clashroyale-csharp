c85f1ac14c799bb64d894f6a366870cf7ca60c20
Mattia Panni <mattia.panni@studio.unibo.it>
2022-09-13 12:17:43 +0200

    Add tower classes

---
+++ clashroyale/Panni/Source/Model/Towers/KingTower.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+using Panni.Source.Model.Users;
+
+namespace Panni.Source.Model.Towers;
+
+public class KingTower : Tower
+{
+
+    private new const double Range = 100;
+    
+    private KingTower(Vector2 position, double damage, double maxHp, User owner) : base(
+        position, KingTower.Range, damage, maxHp, owner, false)
+    {
+    }
+
+    public static KingTower Create(User owner, Vector2 position)
+    {
+        return owner.CurrentLevel switch
+        {
+            UserLevel.Lvl1 => new KingTower(position, 150, 15000, owner),
+            UserLevel.Lvl2 => new KingTower(position, 200, 16000, owner),
+            UserLevel.Lvl3 => new KingTower(position, 250, 17000, owner),
+            UserLevel.Lvl4 => new KingTower(position, 300, 18000, owner),
+            UserLevel.Lvl5 => new KingTower(position, 350, 19000, owner),
+            _ => new KingTower(position, 150, 15000, owner)
+        };
+    }
+    
+}
--- clashroyale/Panni/Source/Model/Towers/QueenTower.cs
+++ clashroyale/Panni/Source/Model/Towers/QueenTower.cs
@@ -5,7 +5,23 @@ namespace Panni.Source.Model.Towers;
 
 public class QueenTower : Tower
 {
-    private QueenTower(Vector2 position, double range, double damage, double maxHp, User owner, bool isActive) : base(position, range, damage, maxHp, owner, isActive)
+    private new const double Range = 100;
+    
+    private QueenTower(Vector2 position, double damage, double maxHp, User owner) : base(
+        position, QueenTower.Range, damage, maxHp, owner, true)
     {
+    }
+
+    public static QueenTower Create(User owner, Vector2 position)
+    {
+        return owner.CurrentLevel switch
+        {
+            UserLevel.Lvl1 => new QueenTower(position, 100, 12000, owner),
+            UserLevel.Lvl2 => new QueenTower(position, 200, 12500, owner),
+            UserLevel.Lvl3 => new QueenTower(position, 300, 13000, owner),
+            UserLevel.Lvl4 => new QueenTower(position, 400, 13500, owner),
+            UserLevel.Lvl5 => new QueenTower(position, 500, 14000, owner),
+            _ => new QueenTower(position, 100, 12000, owner)
+        };
     }
 }
