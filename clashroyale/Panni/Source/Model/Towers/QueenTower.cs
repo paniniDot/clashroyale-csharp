7e134317bc705a0fd2b10d66461bab62c3a77477
Mattia Panni <mattia.panni@studio.unibo.it>
2022-09-13 11:53:46 +0200

    Add towers

---
+++ clashroyale/Panni/Source/Model/Towers/QueenTower.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+using Panni.Source.Model.Users;
+
+namespace Panni.Source.Model.Towers;
+
+public class QueenTower : Tower
+{
+    private QueenTower(Vector2 position, double range, double damage, double maxHp, User owner, bool isActive) : base(position, range, damage, maxHp, owner, isActive)
+    {
+    }
+}
